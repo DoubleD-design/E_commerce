cd5b4d9e3c6294983ccce00002dce2fdf4df1986
duy <doubled020504@gmail.com>
2024-07-13 00:07:53 +0700

    Second commit - HomePage part1

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Navigation from './customer/component/Navigation/Navigation';
+import HomePage from './customer/pages/HomePage/HomePage';
+
+function App() {
+  return (
+    <div className="">
+      <Navigation />
+      <HomePage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/customer/pages/HomePage/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import MainCarousel from '../../component/HomeCarousel/MainCarousel'
+
+const HomePage = () => {
+  return (
+    <div>
+     <MainCarousel/>
+     <div>
+        Other section
+     </div>
+    </div>
+  )
+}
+
+export default HomePage

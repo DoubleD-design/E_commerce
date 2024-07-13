d78b8fd63533434aaf073dca0254547f62ad35a4
duy <doubled020504@gmail.com>
2024-07-13 12:46:49 +0700

    Finish Home Carousel

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-import Navigation from './customer/component/Navigation/Navigation';
-import HomePage from './customer/pages/HomePage/HomePage';
+import logo from "./logo.svg";
+import "./App.css";
+import Navigation from "./customer/component/Navigation/Navigation";
+import HomePage from "./customer/pages/HomePage/HomePage";
 
 function App() {
   return (
     <div className="">
       <Navigation />
-      <HomePage/>
+      <HomePage />
     </div>
   );
 }
--- src/customer/component/HomeCarousel/MainCarousel.jsx
+++ src/customer/component/HomeCarousel/MainCarousel.jsx
@@ -1,22 +1,20 @@
-import React from 'react';
-import AliceCarousel from 'react-alice-carousel';
-import 'react-alice-carousel/lib/alice-carousel.css';
+import React from "react";
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
+import { mainCarouselData } from "./MainCarouselData";
 
-
-const items = [
-    <div className="item" data-value="1">1</div>,
-    <div className="item" data-value="2">2</div>,
-    <div className="item" data-value="3">3</div>,
-    <div className="item" data-value="4">4</div>,
-    <div className="item" data-value="5">5</div>,
-];
-
-const MainCarousel = () => (
-    <AliceCarousel
-        mouseTracking
-        items={items}
-        controlsStrategy="alternate"
+const MainCarousel = () => {
+  const items = mainCarouselData.map((item) => (
+    <img
+      className="cursor-pointer"
+      role="presentation"
+      src={item.image}
+      alt=""
     />
-);
+  ));
+  return (
+    <AliceCarousel items={items} autoPlay autoPlayInterval={2000} infinite />
+  );
+};
 
 export default MainCarousel;

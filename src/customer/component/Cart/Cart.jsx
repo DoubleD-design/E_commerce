12c14830b2903d11f58e37e965084b24a60bcc45
duy <doubled020504@gmail.com>
2024-07-17 14:14:18 +0700

    Cart

--- src/customer/component/Cart/Cart.jsx
+++ src/customer/component/Cart/Cart.jsx
@@ -1,12 +1,72 @@
-import React from 'react'
-import CartItem from './CartItem'
+import React from "react";
+import CartItem from "./CartItem";
+import { styled } from "@mui/material/styles";
+import Button from "@mui/material/Button";
+
+const StyledButton = styled(Button)(({ theme, color = "primary" }) => ({
+  ":hover": {
+    color: "white",
+    backgroundColor: "#4f46e5",
+  },
+}));
 
 const Cart = () => {
   return (
     <div>
-        <CartItem/>
+      <div className="lg:grid grid-cols-3 lg:px-16 relative">
+        <div className="col-span-2 mb-10">
+          {[1, 1, 1, 1].map((item, index) => (
+            <div key={index} className="mb-5">
+              {" "}
+              {/* Add margin-bottom to each CartItem */}
+              <CartItem />
+            </div>
+          ))}
+        </div>
+        <div className="px-5 lg:sticky lg:top-0 h-[100vh] mt-5 lg:mt-0">
+          {" "}
+          {/* Use sticky directly here */}
+          <div className="border px-5 py-5" style={{ borderRadius: "5px" }}>
+            <p className="uppercase font-bold opacity-60 pb-4">Price Details</p>
+            <hr />
+            <div className="space-y-2 font-semibold">
+              <div className="flex justify-between pt-1 text-black">
+                <span>Price</span>
+                <span>$1499</span>
+              </div>
+              <div className="flex justify-between pt-1 text-green-600">
+                <span className="text-black">Discount</span>
+                <span>- $1000</span>
+              </div>
+              <div className="flex justify-between pt-1 text-green-600">
+                <span className="text-black">Delivery Charge</span>
+                <span>Free</span>
+              </div>
+              <hr />
+              <div className="flex justify-between font-bold pt-1 text-green-600 pb-5">
+                <span className="text-black text-lg">Total Amount</span>
+                <span>$499</span>
+              </div>
+              <StyledButton
+                variant="contained"
+                sx={{
+                  px: "2rem",
+                  py: "1rem",
+                  bgcolor: "#6366f1",
+                  marginTop: "1rem",
+                  marginBottom: "1rem",
+                  width: "100%",
+                  fontSize: "100%",
+                }}
+              >
+                Purchase
+              </StyledButton>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Cart
+export default Cart;
--- src/customer/component/Cart/CartItem.jsx
+++ src/customer/component/Cart/CartItem.jsx
@@ -1,5 +1,15 @@
 import React from "react";
-
+import IconButton from "@mui/material/IconButton";
+import RemoveCircleOutlineIcon from "@mui/icons-material/RemoveCircleOutline";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import Button from "@mui/material/Button";
+import { styled } from "@mui/material/styles";
+const StyledButton = styled(Button)(({ theme, color = "primary" }) => ({
+  ":hover": {
+    color: theme.palette[color].main,
+    backgroundColor: "white",
+  },
+}));
 const CartItem = () => {
   return (
     <div className="p-5 shadow-lg border rounded-md">
@@ -17,11 +27,30 @@ const CartItem = () => {
           </p>
           <p className="opacity-70">Size: L, White</p>
           <p className="opacity-70 mt-2">Seller: SG LEMAN</p>
-          <div className="flex space-x-5 items-center text-lg lg:text-xl text-gray-900 mt-6">
+          <div className="flex space-x-5 items-center text-gray-900 pt-6">
             <p className="font-semibold">$499</p>
             <p className="opacity-50 line-through">$1499</p>
             <p className="text-green-600 font-semibold">66% Off</p>
           </div>
+        </div>
+      </div>
+      <div className="lg:flex items-center lg:space-x-10 pt-4">
+        <div className="flex items-center space-x-2">
+          <IconButton>
+            <RemoveCircleOutlineIcon />
+          </IconButton>
+          <span
+            className="py-1 px-7 border rounded-sm"
+            style={{ borderRadius: "5px", borderColor: "indiogo" }}
+          >
+            3
+          </span>
+          <IconButton color="secondary">
+            <AddCircleOutlineIcon />
+          </IconButton>
+        </div>
+        <div>
+          <Button color="secondary">Remove</Button>
         </div>
       </div>
     </div>
